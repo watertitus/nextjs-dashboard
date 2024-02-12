6233fe4fb825e662defe1e0872de736dadb7ec59
unknown <ot.olowoyeye@gmail.com>
2024-02-12 12:09:16 +0100

    progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+ 
+        {children}
+               <h1>Testing</h1></body>
     </html>
   );
 }
